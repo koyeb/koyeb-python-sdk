7adbc7684c8c85c6102d944160598325e56668b0
Bastien Chatelard <bchatelard@users.noreply.github.com>
2026-04-03 21:28:45 +0200

    Post release version bump

--- koyeb/__init__.py
+++ koyeb/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 # Make Sandbox available at package level
 from .sandbox import Sandbox, AsyncSandbox
--- koyeb/api/__init__.py
+++ koyeb/api/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 # Define package exports
 __all__ = [
